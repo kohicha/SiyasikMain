10ef6ad438906822f65821610c2723d1757299e2
kohicha <yensyd.francisco@gmail.com>
2023-12-03 15:54:29 +0800

    upd env

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const expressLayouts = require('express-ejs-layouts');
 const session = require('express-session')
 const app = express()
 require('dotenv').config();
-
+const { createClient } = require('@supabase/supabase-js')
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 
 
+//initialize db
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+
 //set ports
 const port = process.env.PORT;
-
-
 
 //default file routing
 app.use(express.static('public'))
