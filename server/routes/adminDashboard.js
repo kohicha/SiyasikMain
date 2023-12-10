4c8641fec4592aada34babf0b9b1ca748ec8e07d
kohicha <yensyd.francisco@gmail.com>
2023-12-10 16:10:51 +0800

    Update adminDashboard.js

--- server/routes/adminDashboard.js
+++ server/routes/adminDashboard.js
@@ -17,7 +17,9 @@ router.post('/admin', upload.single('productImage'), async (req, res) => {
         const {data, error} = await supabase
         .storage
         .from('images')
-        .upload(`products/` + uuidv4, productImageFile.buffer)
+        .upload(`products/` + uuidv4, productImageFile.buffer ,{
+            contentType: "image/png, image/jpg, image/jpeg"
+        })
 
         if(error){
             console.log('Error!', error)
